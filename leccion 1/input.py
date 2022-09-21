f6afdb5e61539187aa96e9ecd6d42a2e65f707fc
Jorge Daniel <gamingprm1@gmail.com>
2022-09-21 11:34:22 -0400

    commit probando

--- leccion 1/input.py
+++ leccion 1/input.py
@@ -5,5 +5,3 @@ numero1 = int(input('Escribe el primer numero: '))
 numero2 = int(input('Escribe el segundo numero: '))
 resultado = numero1 + numero2
 print('El resultado de la suma es: ', resultado)
-
-
