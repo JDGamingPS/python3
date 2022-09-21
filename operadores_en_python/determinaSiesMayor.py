f73f1f8f01d08ad46ce8dd4dfa5c428745a88a52
JDGamingPS <gamingprm1@gmail.com>
2022-09-20 22:34:46 -0400

    aprendiendo python 3

---
+++ operadores_en_python/determinaSiesMayor.py
@@ -0,0 +1,8 @@
+edadAdulto = 18
+edadpersona = int(input('proporcina tu edad'))
+
+if edadpersona >= edadAdulto:
+    print(f'Lapersona con edad {edadpersona} es un adulto')
+else:
+    print(f'Lapersona con edad {edadpersona} es un menor de edad')
+
---
+++ operadores_en_python/ejercicioParImpar.py
@@ -0,0 +1,6 @@
+a = int(input('Escribe un valor numerico'))
+
+if a % 2 == 0:
+    print(f'El numero de a {a} es número par')
+else:
+    print(f'el valor de {a} es número impar')
